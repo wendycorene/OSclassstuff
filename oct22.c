24b8b2d67ef47d5723f2898b30dbf3777f08f66a
Wendy Eloe <s524779@nwmissouri.edu>
2018-10-22 13:32:40 -0500

    working

--- oct22.c
+++ oct22.c
@@ -7,7 +7,8 @@ int bad_idea = 666;
 
 int main(int argc, char* argv[])
 {
-  srand(time(NULL));
+  //srand(time(NULL));
+  srand(getpid());
   int c = fork();
   bad_idea += rand() % 100;
   printf("%d %d %d\n", getpid(), c, bad_idea);
