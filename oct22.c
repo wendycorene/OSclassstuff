d1651dc328b163017fdc73bb5e3bba9e7fc5ac08
Wendy Eloe <s524779@nwmissouri.edu>
2018-10-22 13:27:35 -0500

    working

--- oct22.c
+++ oct22.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int bad_idea = 666;
+
 int main(int argc, char* argv[])
 {
   int c = fork();
-  printf("%d %d\n", getpid(), c);
+  printf("%d %d\n", getpid(), c, bad_idea);
   return 0;
 }
