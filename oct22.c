a0525ab03cf5cbb4e54b97e33a1b983ee03cf3a5
Wendy Eloe <s524779@nwmissouri.edu>
2018-10-22 13:29:41 -0500

    working

--- oct22.c
+++ oct22.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 int bad_idea = 666;
 
 int main(int argc, char* argv[])
 {
+  srand(time(NULL));
   int c = fork();
-  printf("%d %d\n", getpid(), c, bad_idea);
+  bad_idea += rand() % 100;
+  printf("%d %d %d\n", getpid(), c, bad_idea);
   return 0;
 }
