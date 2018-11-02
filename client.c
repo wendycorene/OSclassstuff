d0f98c2acd401f97c817c22306b19f59e1066e39
Wendy Eloe <s524779@nwmissouri.edu>
2018-11-02 13:41:33 -0500

    added server.c and client.c

---
+++ client.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netdb.h>
+
+int sock;
+
+void handler(int sig);
+
+int main(int argc, char* argv[])
+{
+  sock = socket (AF_INET, SOCK_STREAM, 0);
+  signal(SIGINT, handler);
+  struct sockaddr_in server;
+  struct hostent * host;
+  char mesg[] = "hello world";
+  char input(100);
+  host = gethostbyname (argv[1]);
+
+  if (!host)
+  {
+    printf("host not found"\n");
+    exit(1);
+  }
+
+  if (sock == -1)
+  {
+    printf("error creating socket. exiting\n");
+    exit(1);
+  }
+
+  server.sin_family = AF_INET;
+  server.sin_port = htons(9999);
+  memcpy(&server,sub_add, host->h_addr, host->h_length);
+
+  if (connect(sock, (struct sockaddr*) &my_address, sizeof(struct sockaddr_in)) == -1)
+  {
+    printf("Error connecting the sock.\n");
+    close(sock);
+    exit(1);
+  }
+
+  char* input2 = calloc(100, sizeof(char));
+  size_t read_in = 100;
+  while (1)
+  {
+    getline(&input2, &readin, strlen(input)+1);
+    read(sock, input, 100);
+    printf("client got: %s\n", input);
+  }
+  close(sock);
+
+  return 0;
+}
+
+void handler (int sig)
+{
+  close(sock);
+  exit(0);
+}
---
+++ server.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netdb.h>
+
+int sock;
+int client;
+
+void handler(int sig);
+
+int main (int argc, char* argv[])
+{
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  signal (SIGINT, handler);
+  int i;
+  char data[256] = {};
+  int size;
+  struct sockaddr_in my_address;
+  my_address.sin_family = AF_INET;
+  my_address.sin_port = htons(9999);
+  my_address.sin_addr.s_addr = INADDR_ANY;
+
+  if (sock == -1)
+  {
+    printf("Error creating socket. exiting\n");
+    exit(1);
+  }
+
+  if (bind(sock, (struct sockaddr*) &my_address, sizeof(struct sockaddr_in)) == -1)
+  {
+    printf("Error binding the sock.\n");
+    close(sock);
+    exit(1);
+  }
+
+  if (listen(sock,10) == -1)
+  {
+    printf("Listen errored, exiting.\n");
+    close(sock);
+    exit(1);
+  }
+
+  client = accept(sock, NULL, NULL);
+  char * input = calloc(100, sizeof(char));
+  size_t readin = 100;
+  while (1)
+  {
+    read(client, data, 256);
+    printf("Server got: %s\n", data);
+    getline(&input, &readin, stdin);
+    write(client, input, strlen(input)+1);
+  }
+  close (sock);
+  close (client);
+
+  return 0;
+}
+
+void handler (int sig)
+{
+  close(sock);
+  close(client);
+  exit(0);
+}
